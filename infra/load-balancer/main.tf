533a10083c281c1c10014aa2b37e7a804edc9b40
Ngozi-N <jonathanengee@gmail.com>
2025-02-17 14:47:45 +0100

    Update to file structure

--- infra/load-balancer/main.tf
+++ infra/load-balancer/main.tf
@@ -1,55 +1,55 @@
-module "networking" {
-  source               = "/networking"
-  vpc_cidr             = var.vpc_cidr
-  vpc_name             = var.vpc_name
-  cidr_public_subnet   = var.cidr_public_subnet
-  eu_availability_zone = var.eu_availability_zone
-  cidr_private_subnet  = var.cidr_private_subnet
+variable "lb_name" {}
+variable "lb_type" {}
+variable "is_external" { default = false }
+variable "sg_enable_ssh_https" {}
+variable "subnet_ids" {}
+variable "tag_name" {}
+variable "lb_target_group_arn" {}
+variable "ec2_instance_id" {}
+variable "lb_listner_port" {}
+variable "lb_listner_protocol" {}
+variable "lb_listner_default_action" {}
+variable "lb_https_listner_port" { default = null }
+variable "lb_https_listner_protocol" { default = null }
+variable "dev_proj_1_acm_arn" { default = null }
+variable "lb_target_group_attachment_port" {}
+
+output "aws_lb_dns_name" {
+  value = aws_lb.dev_proj_1_lb.dns_name
 }
 
-module "security_group" {
-  source              = "/security-groups"
-  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
-  vpc_id              = module.networking.dev_proj_1_vpc_id
-  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+output "aws_lb_zone_id" {
+  value = aws_lb.dev_proj_1_lb.zone_id
 }
 
-/*module "jenkins" {
-  source                    = "./jenkins"
-  ami_id                    = var.ec2_ami_id
-  instance_type             = "t2.medium"
-  tag_name                  = "Jenkins:Ubuntu Linux EC2"
-  public_key                = var.public_key
-  subnet_id                 = tolist(module.networking.dev_proj_1_public_subnets)[0]
-  sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_jenkins_port_8080]
-  enable_public_ip_address  = true
-  user_data_install_jenkins = templatefile("./jenkins-runner-script/jenkins-installer.sh", {})
-}*/
 
-module "lb_target_group" {
-  source                   = "/load-balancer-target-group"
-  lb_target_group_name     = "jenkins-lb-target-group"
-  lb_target_group_port     = 8080
-  lb_target_group_protocol = "HTTP"
-  vpc_id                   = module.networking.dev_proj_1_vpc_id
-  ec2_instance_id          = module.jenkins.jenkins_ec2_instance_ip
+resource "aws_lb" "dev_proj_1_lb" {
+  name               = var.lb_name
+  internal           = var.is_external
+  load_balancer_type = var.lb_type
+  security_groups    = [var.sg_enable_ssh_https]
+  subnets            = var.subnet_ids # Replace with your subnet IDs
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "example-lb"
+  }
 }
 
-module "alb" {
-  source                    = "/load-balancer"
-  lb_name                   = "dev-proj-1-alb"
-  is_external               = false
-  lb_type                   = "application"
-  sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
-  subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
-  tag_name                  = "dev-proj-1-alb"
-  lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
-  ec2_instance_id           = module.jenkins.jenkins_ec2_instance_ip
-  lb_listner_port           = 80
-  lb_listner_protocol       = "HTTP"
-  lb_listner_default_action = "forward"
-  lb_target_group_attachment_port = 8080
-  #lb_https_listner_port     = 443
-  #lb_https_listner_protocol = "HTTPS"
-  #dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_1_acm_arn
+resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment" {
+  target_group_arn = var.lb_target_group_arn
+  target_id        = var.ec2_instance_id # Replace with your EC2 instance reference
+  port             = var.lb_target_group_attachment_port
+}
+
+resource "aws_lb_listener" "dev_proj_1_lb_listner" {
+  load_balancer_arn = aws_lb.dev_proj_1_lb.arn
+  port              = var.lb_listner_port
+  protocol          = var.lb_listner_protocol
+
+  default_action {
+    type             = var.lb_listner_default_action
+    target_group_arn = var.lb_target_group_arn
+  }
 }
